e8feef4feb8f8090bfe75bd9dcdbd901d07af0f1
Ashish Anand <aashishanand07@gmail.com>
2025-07-13 22:52:00 +0530

    middleware

--- middleware-example.js
+++ middleware-example.js
@@ -3,6 +3,12 @@ const express = require("express");
 const app = express();
 
 
+// An example where we can use middlewares is authentication. 
+// Suppose if we have an admin dashboard, from where we have to call some API, 
+// every time we have to check whether it's an admin or not.
+// Here we can use middlewares
+
+
 
 app.listen(3000, () => {
     console.log("server connected");
