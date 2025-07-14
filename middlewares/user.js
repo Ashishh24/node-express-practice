308165664e259d1f2f1f48ff8926bff144cb7fe1
ashish-anand-si <ashish.anand@singleinterface.com>
2025-07-14 11:24:35 +0530

    middlewares

---
+++ middlewares/user.js
@@ -0,0 +1,13 @@
+const userAuth = (req, res, next) => {
+    const token = "xyz";
+    const isUserAuthorized = token === "xyz";
+    if(!isUserAuthorized){
+        res.status(401).send("Unautorized User");
+    }
+    
+    else{
+        next();
+    }
+};
+
+module.exports = {userAuth};
