a2b44ea68b2228ff0a8cf40d42287349086b0e43
Ashish Anand <aashishanand07@gmail.com>
2025-07-16 23:27:11 +0530

    learning

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const connectDB = require("./database");
 
 const app = express();
 
+const User = require("./models/schema");
 
-
-app.listen(3000, () => {
-    console.log("server test connected");
+connectDB().then(() => {
+    console.log("DB connected :)");
+    app.listen(3000, () => {
+        console.log("server test connected");
+    });
+}).catch((err) => {
+    console.log(err);
+    console.log("DB connected :(");
 });
