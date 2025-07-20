f02d7a6a5824f3dab4c51d66138077ce83505e2a
Ashish Anand <aashishanand07@gmail.com>
2025-07-20 20:05:54 +0530

    login and profile auth using middleware

--- middlewares/user.js
+++ middlewares/user.js
@@ -1,12 +1,23 @@
-const userAuth = (req, res, next) => {
-    const token = "xyz";
-    const isUserAuthorized = token === "xyz";
-    if(!isUserAuthorized){
-        res.status(401).send("Unautorized User");
-    }
-    
-    else{
+const jwt = require("jsonwebtoken");
+const User = require("../models/schema");
+
+const userAuth = async (req, res, next) => {
+    try{
+        const cookie = req.cookies;
+        const {token} = cookie;
+        
+        if(!token){
+            throw new Error("Invalid Token!");
+        }
+        const decodedId = jwt.verify(token, 'Link@in@1804');
+        const {_id} = decodedId; 
+
+        const user = await User.findById(_id);
+        req.user = user;
         next();
+    }
+    catch(err) {
+        res.status(402).send("ERROR: " + err.message);
     }
 };
 
---
+++ profileAuthMiddleware.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const connectDB = require("./database");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
+const {userAuth} = require("./middlewares/user");
+
+const app = express();
+
+const User = require("./models/schema");
+
+app.use(express.json()); // a middleware that converts all json to js object
+app.use(cookieParser()); //Get cookie from web
+
+app.post("/login", async (req, res) => {
+    try{
+        const {email, password} = req.body;
+        const isUser = await User.findOne({email: email});
+
+        if(!isUser){
+            throw new Error("Invalid email!!")
+        }
+        const isPasswordValid = await bcrypt.compare(password, isUser.password)
+        if(!isPasswordValid){
+            throw new Error("Invalid password!!");
+        }
+        else{
+            var token = jwt.sign({ _id: isUser._id }, 'Link@in@1804');
+            res.cookie("token", token);
+            res.send("Login Successful!!!!😊")
+        }
+    }
+    catch(err) {
+        res.status(402).send("ERROR: " + err.message);
+    }
+});
+
+app.get("/profile", userAuth, async (req, res) => {
+    try{
+        user = req.user;
+        if(!user){
+            throw new Error("Invalid Userr!!")
+        }
+        res.send(user);
+    }
+    catch(err) {
+        res.status(402).send("ERROR: " + err.message);
+    }
+})
+
+app.get("/sendConnectionRequet", userAuth, (req, res) => {
+    // res.send("Connection Request Sent!!")
+    const user = req.user;
+    res.send(user.firstName + " " + user.lastName + " sent a connection requet!")
+})
+
+connectDB().then(() => {
+    console.log("DB connected :)");
+    app.listen(3000, () => {
+        console.log("server test connected");
+    });
+}).catch((err) => {
+    console.log(err);
+    console.log("DB connected :(");
+});
